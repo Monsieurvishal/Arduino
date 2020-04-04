77f552171390ffbecc18bebd7834ddc08ef90824
Vishal Naik <59202862+Monsieurvishal@users.noreply.github.com>
2020-04-04 23:08:39 +0530

    Update pir_sensor.ino

--- pir_sensor.ino
+++ pir_sensor.ino
@@ -4,7 +4,7 @@ pinMode(6,INPUT);
 Serial.begin(9600);   
 
 }
-
+//PIR sensor is the passive infrared sensor which works on he principle of detecting the change in heat due to the reflection of IR rays
 void loop() 
 {
   int pir=digitalRead(6);
